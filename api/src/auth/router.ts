b3a8d1aad426e6a1e669bbe051eaadb848850d3b
Arkadiusz-Wrzawinski-DNA <arkadiusz.wrzawinski@digitalnewagency.com>
2025-03-13 15:17:41 +0100

    CORS

--- api/src/auth/router.ts
+++ api/src/auth/router.ts
@@ -3,7 +3,19 @@ import { db } from './db';
 
 const authRouter = Router();
 
+authRouter.options('/login', (request: Request, response: Response) => {
+  response.setHeader('Access-Control-Allow-Origin', '*');
+  response.setHeader('Access-Control-Allow-Methods', '*');
+  response.setHeader('Access-Control-Allow-Headers', '*');
+
+  response.end()
+})
+
 authRouter.post('/login', (request: Request, response: Response) => {
+  response.setHeader('Access-Control-Allow-Origin', '*');
+  response.setHeader('Access-Control-Allow-Methods', '*');
+  response.setHeader('Access-Control-Allow-Headers', '*');
+
     const user = db.getAllUsers().find(({ email, password }) =>
         request.body.email === email &&
         request.body.password === password
